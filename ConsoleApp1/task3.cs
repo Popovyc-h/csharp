1f4faf48b1e581ce2e1d3031000345bb9b72b76c
Popovyc-h <popovichtanja86@gmail.com>
2025-11-21 19:13:45 +0200

    Implement number input and calculation logic

---
+++ ConsoleApp1/task3.cs
@@ -0,0 +1,14 @@
+Console.Write("Enter number 1: ");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number 2: ");
+int n2 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number 3: ");
+int n3 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number 4: ");
+int n4 = int.Parse(Console.ReadLine());
+
+int result = n1 * 1000 + n2 * 100 + n3 * 10 + n4 * 1;
+Console.WriteLine(result);
