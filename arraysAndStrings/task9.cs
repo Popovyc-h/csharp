9994b8a5d0c9c79060c420a6e6dddd424df34e62
Ivan <popovichtanja86@gmail.com>
2025-11-22 22:34:59 +0200

    .

--- arraysAndStrings/task9.cs
+++ arraysAndStrings/task9.cs
@@ -1,1 +1,27 @@
+int rows;
+int columns;
+int multiplier;
+Random rand = new Random();
 
+Console.Write("Enter size rows: ");
+rows = int.Parse(Console.ReadLine());
+
+Console.Write("Enter size columns: ");
+columns = int.Parse(Console.ReadLine());
+
+Console.Write("Enter multiplier: ");
+multiplier = int.Parse(Console.ReadLine());
+
+int[,] array = new int[rows, columns];
+
+for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = rand.Next(1, 11);
+
+Console.WriteLine("\nArray:");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] * multiplier + " ");
+    Console.WriteLine();
+}
