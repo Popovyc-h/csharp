28e2e7b6c22f3a9ae1e7ace9d9cfba0807bb8f52
Ivan <popovichtanja86@gmail.com>
2025-11-22 22:00:15 +0200

    .

--- arraysAndStrings/task6.cs
+++ arraysAndStrings/task6.cs
@@ -1,1 +1,30 @@
 
+using System.Text;
+
+string text;
+Console.Write("Enter text: ");
+text = Console.ReadLine();
+
+bool makeUpper = true;
+
+Console.WriteLine("Original text:");
+Console.WriteLine(text);
+
+StringBuilder sb = new StringBuilder();
+
+for (int i = 0; i < text.Length; i++)
+{
+    if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        makeUpper = true;
+
+    if (makeUpper == true && char.IsLetter(text[i]))
+    {
+        sb.Append(char.ToUpper(text[i]));
+        makeUpper = false;
+    }
+    else
+        sb.Append(text[i]);
+}
+
+Console.WriteLine("\nEdited text:");
+Console.WriteLine(sb.ToString());
