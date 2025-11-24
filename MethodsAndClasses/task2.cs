ca978ef7f1a15a1944ec47eadbac2dced6a75eb7
Popovyc-h <popovichtanja86@gmail.com>
2025-11-24 16:10:43 +0200

    Add palindrome check functionality

--- MethodsAndClasses/task2.cs
+++ MethodsAndClasses/task2.cs
@@ -1,1 +1,25 @@
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
 
+if (isPalindrome(number))
+    Console.WriteLine("palindrome!");
+else
+    Console.WriteLine("not a palindrome");
+
+bool isPalindrome(int n)
+{
+    if (n < 0) 
+        return false;
+
+    int original = n;
+    int reversed = 0;
+
+    while (n > 0)
+    {
+        int digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n /= 10;
+    }
+
+    return original == reversed;
+}
