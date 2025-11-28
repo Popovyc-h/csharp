d9ca4509a52834c1238bd9f4c575e1e9b10fd1ee
Popovyc-h <popovichtanja86@gmail.com>
2025-11-29 01:17:23 +0200

    Add Caesar cipher encryption logic
    Implement Caesar cipher encryption for input text.

--- arraysAndStrings/task8.cs
+++ arraysAndStrings/task8.cs
@@ -1,1 +1,42 @@
+int shift;
+string text = "";
+string result = "";
 
+if (args.Length < 2)
+{
+    Console.WriteLine("Not enough arguments");
+    return;
+}
+
+shift = int.Parse(args[0]);
+text = args[1];
+
+for (int i = 0; i < text.Length; i++)
+{
+    char ch = text[i];
+
+    if (char.IsUpper(ch))
+    {
+        int index = ch - 'A';
+
+        int newIndex = index + shift;
+        newIndex %= 26;
+
+        char encrypted = (char)('A' + newIndex);
+
+        result += encrypted;
+    }
+    else if (char.IsLower(ch))
+    {
+        int index = ch - 'a';
+
+        int newIndex = index + shift;
+        newIndex %= 26;
+
+        char encrypted = (char)('a' + newIndex);
+
+        result += encrypted;
+    }
+}
+
+Console.WriteLine(result);
