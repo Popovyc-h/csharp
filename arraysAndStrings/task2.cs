1b449895795677bb1d8a071b2e7c689359f4d2a0
Popovyc-h <popovichtanja86@gmail.com>
2025-11-29 10:34:41 +0200

    Initialize array, find min/max, and calculate sum
    This code initializes a 5x5 array with random integers, finds the minimum and maximum values along with their indices, and calculates the sum of the elements between these indices.

--- arraysAndStrings/task2.cs
+++ arraysAndStrings/task2.cs
@@ -1,1 +1,64 @@
+using System;
 
+int[,] array = new int[5, 5];
+Random rand = new Random();
+
+for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = rand.Next(-101, 101);
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + " ");
+    Console.WriteLine();
+}
+
+int minValue = array[0, 0];
+int maxValue = array[0, 0];
+int minI = 0;
+int minJ = 0;
+int maxI = 0;
+int maxJ = 0;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if(minValue > array[i, j])
+        {
+            minValue = array[i, j];
+            minI = i;
+            minJ = j;
+        }
+
+        if (maxValue < array[i, j])
+        {
+            maxValue = array[i, j];
+            maxI = i;
+            maxJ = j;
+        }
+    }
+}
+
+int minIndex = minI * 5 + minJ;
+int maxIndex = maxI * 5 + maxJ;
+int temp;
+int sum = 0;
+
+if (minIndex > maxIndex)
+{     
+    temp = minIndex;
+    minIndex = maxIndex;
+    maxIndex = temp;
+}
+
+for (int i = minIndex + 1; i < maxIndex; i++)
+{
+    int row = i / 5;
+    int col = i % 5;
+
+    sum += array[row, col];
+}
+
+Console.WriteLine($"Sum of array elements: {sum}");
