f10fdf88ee55801fe9811fe82f84157ea4ba4d9e
Popovyc-h <popovichtanja86@gmail.com>
2025-11-29 11:01:54 +0200

    Implement basic calculator for addition and subtraction
    This code reads a mathematical expression from user input, processes it by removing spaces, and evaluates the result of addition and subtraction operations.

--- arraysAndStrings/task5.cs
+++ arraysAndStrings/task5.cs
@@ -1,1 +1,43 @@
+Console.Write("Enter text: ");
+string text = Console.ReadLine();
 
+text = text.Replace(" ", "");
+
+int pos = 0;
+
+for (int i = 0; i < text.Length; i++)
+    if (text[i] == '+' || text[i] == '-')
+    {
+        pos = i;
+        break;
+    }
+
+string firstNumber = text.Substring(0, pos);
+
+int result = int.Parse(firstNumber);
+
+for (int i = pos; i < text.Length; i++)
+{
+    if (text[i] == '+' || text[i] == '-')
+    {
+        char sign = text[i];
+        int j = i + 1;
+        string number = "";
+        while (j < text.Length && char.IsDigit(text[j]))
+        {
+            number += text[j];
+            j++;
+        }
+
+        int value = int.Parse(number);
+        
+        i = j - 1;
+
+        if (sign == '+')
+            result += value;
+        else
+            result -= value;
+    }
+}
+
+Console.WriteLine(result);
