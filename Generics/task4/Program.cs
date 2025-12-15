b09d3175a9eaaf8015d9bc8629a468f8f18ec48f
Popovyc-h <popovichtanja86@gmail.com>
2025-12-15 18:13:44 +0200

    Update Program.cs

--- Generics/task4/Program.cs
+++ Generics/task4/Program.cs
@@ -1,1 +1,24 @@
+namespace SinglyLinkedList;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var n = new SinglyLinkedList<int>();
+
+        n.AddFirst(2);
+        n.AddFirst(1);
+        n.AddFirst(9);
+        n.AddLast(43);
+
+        n.Print();
+        
+        n.RemoveFirst();
+     
+        n.Print();
+
+        n.RemoveLast();
+
+        n.Print();
+    }
+}
