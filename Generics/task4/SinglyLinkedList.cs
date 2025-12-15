763afb7fefb5071737ef385f3181cbc87c059d78
Popovyc-h <popovichtanja86@gmail.com>
2025-12-15 18:14:01 +0200

    Update SinglyLinkedList.cs

--- Generics/task4/SinglyLinkedList.cs
+++ Generics/task4/SinglyLinkedList.cs
@@ -1,1 +1,78 @@
+namespace SinglyLinkedList
+{
+    internal class SinglyLinkedList<T>
+    {
+        public Node<T> head;
 
+        public SinglyLinkedList()
+        {
+            head = null;
+        }
+
+        public void AddFirst(T value)
+        {
+            var newNode = new Node<T>(value);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        public void AddLast(T value)
+        {
+            var newNode = new Node<T>(value);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node<T> current = head;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+
+        public void RemoveFirst()
+        {
+            if (head == null)
+                throw new InvalidOperationException("List is empty");
+
+            head = head.Next;
+        }
+
+        public void RemoveLast()
+        {
+            if (head == null)
+                throw new InvalidOperationException("List is empty");
+
+            if (head.Next == null)
+            {
+                head = null;
+                return;
+            }
+
+            Node<T> current = head;
+
+            while (current.Next.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = null;
+        }
+
+        public void Print()
+        {
+            Node<T> current = head;
+
+            while (current != null)
+            {
+                Console.Write($"{current.Value} ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
