605709531a655e8ee039e8ce90ab8c81db30f3ad
Popovyc-h <popovichtanja86@gmail.com>
2025-12-15 18:14:19 +0200

    Add Node class for singly linked list implementation

---
+++ Generics/task4/Node.cs
@@ -0,0 +1,14 @@
+namespace SinglyLinkedList
+{
+    internal class Node<T>
+    {
+        public T Value;
+        public Node<T> Next;
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+}
