67bd3ddbeeb4b197c9c2815216eca77cac52e555
Popovyc-h <popovichtanja86@gmail.com>
2025-12-16 20:31:44 +0200

    Implement main method for DoublyLinkedList operations
    Demonstrate usage of DoublyLinkedList with basic operations.

--- Generics/task5/Program.cs
+++ Generics/task5/Program.cs
@@ -1,1 +1,25 @@
+namespace DoublyLinkedList;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var list = new DoublyLinkedList<int>();
+
+        list.AddFirst(10);
+        list.AddFirst(12);
+        list.AddFirst(14);
+        list.AddFirst(16);
+
+        list.Print();
+
+        list.AddLast(0);
+
+        list.Print();
+
+        list.RemoveFirst();
+        list.RemoveLast();
+
+        list.Print();
+    }
+}
