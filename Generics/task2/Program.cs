de008df7602fc8c579034656af1ba56a0c5570f1
Popovyc-h <popovichtanja86@gmail.com>
2025-12-17 18:17:03 +0200

    Update Program.cs

--- Generics/task2/Program.cs
+++ Generics/task2/Program.cs
@@ -1,1 +1,25 @@
+namespace QueueWithPriorities;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var queue = new PriorityQueue<int>();
+
+        queue.Enqueue(5, 6);
+        queue.Enqueue(20, 3);
+        queue.Enqueue(10, 1);
+
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
+        try
+        {
+            Console.WriteLine(queue.Dequeue());
+        }
+        catch(InvalidOperationException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
