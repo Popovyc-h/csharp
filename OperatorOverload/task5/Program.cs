0d4a9665cc2c9544f7d03f8f33645c9bd0530131
Popovyc-h <popovichtanja86@gmail.com>
2025-12-20 11:20:20 +0200

    Update Program.cs

--- OperatorOverload/task5/Program.cs
+++ OperatorOverload/task5/Program.cs
@@ -1,1 +1,29 @@
+namespace Fraction;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var a = new Fraction(1, 2).Reduce();
+        var b = new Fraction(2, 4).Reduce();
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        var sum = a + b;
+        Console.WriteLine($"Sum = {sum}");
+
+        var diff = a - b;
+        Console.WriteLine($"Diff = {diff}");
+
+        var mul = a * b;
+        Console.WriteLine($"Mul = {mul}");
+
+        var div = a / b;
+        Console.WriteLine($"Div = {div}");
+
+        Console.WriteLine($"a == b: {a == b}");
+
+        Console.WriteLine($"a != b: {a != b}");
+    }
+}
