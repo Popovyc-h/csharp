fcc1af8f14e278674a9c1a900e4f91e416ad3db3
Popovyc-h <popovichtanja86@gmail.com>
2025-12-21 16:19:03 +0200

    Update Program.cs

--- OperatorOverload/task3/Program.cs
+++ OperatorOverload/task3/Program.cs
@@ -1,1 +1,18 @@
+namespace ListOfBooks;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var myList = new ListOfBooks();
+
+        var book1 = new Book("1984", "George Orwell", 1949);
+        myList.Add(book1);
+        
+        myList = myList + new Book("Кобзар", "Тарас Шевченко", 1840);
+
+        Console.WriteLine($"Кількість книг: {myList.count}");
+
+        Console.WriteLine($"Перша книга: {myList[0].Title}");
+    }
+}
