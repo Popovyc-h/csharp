39e8b0a59377f694a2b6b6b36085ea907bfc9593
Popovyc-h <popovichtanja86@gmail.com>
2026-01-11 16:36:27 +0200

    Create Firm.cs

---
+++ LINQ/Firm/Firm.cs
@@ -0,0 +1,27 @@
+namespace IntroductionToLINQ;
+
+public enum BusinessProfile
+{
+    Marketing,
+    IT
+};
+
+internal class Firm
+{
+    public string companyName;
+    public DateTime foundingDate;
+    public BusinessProfile Profile;
+    public string fullName;
+    public int employeeCount;
+    public string adress;
+
+    public Firm(string companyName, DateTime foundingDate, BusinessProfile Profile, string fullName, int employeeCount, string adress)
+    {
+        this.companyName = companyName;
+        this.foundingDate = foundingDate;
+        this.Profile = Profile;
+        this.fullName = fullName;
+        this.employeeCount = employeeCount;
+        this.adress = adress;
+    }
+}
