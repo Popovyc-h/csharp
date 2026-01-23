a3ba8eb3c5637cc1a303f7716459ae69ba0a74ab
Popovyc-h <popovichtanja86@gmail.com>
2026-01-23 12:17:33 +0200

    Update contact names and improve JSON serialization

--- WorkWithFile/Serialization/task2/Program.cs
+++ WorkWithFile/Serialization/task2/Program.cs
@@ -1,3 +1,7 @@
+using System.Text.Json;
+
+namespace WorkWithFile;
+
 internal class Program
 {
     static void Main(string[] args)
@@ -5,14 +9,19 @@ internal class Program
         List<Contact> contacts = new List<Contact>();
         var path = "contacts.json";
 
-        var firstContact = new Contact("Олег", "067-123-4567");
-        var secondContact = new Contact("Марія", "098-555-2211");
-        var thirdContact = new Contact("Ігор", "095-777-8899");
+        var firstContact = new Contact("Oleg", "067-123-4567");
+        var secondContact = new Contact("Vasily", "098-555-2211");
+        var thirdContact = new Contact("Igor", "095-777-8899");
 
         contacts.AddRange(firstContact, secondContact, thirdContact);
 
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
         Console.WriteLine("=== СЕРІАЛІЗАЦІЯ ===");
-        var jsonString = JsonSerializer.Serialize(contacts);
+        var jsonString = JsonSerializer.Serialize(contacts, options);
 
         File.WriteAllText(path, jsonString);
 
@@ -20,7 +29,7 @@ internal class Program
         string jsonRead = File.ReadAllText(path);
         List<Contact> loaded = JsonSerializer.Deserialize<List<Contact>>(jsonRead);
 
-        foreach (var contact in contacts)
+        foreach (var contact in loaded)
             contact.Print();
     }
 }
