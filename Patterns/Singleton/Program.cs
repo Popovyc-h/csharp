0a6f7e6b80868ed63674a97d452e40c38d243208
Popovyc-h <popovichtanja86@gmail.com>
2026-01-28 14:33:53 +0200

    Implement Singleton pattern in Program.cs

--- Patterns/Singleton/Program.cs
+++ Patterns/Singleton/Program.cs
@@ -1,1 +1,12 @@
+namespace Singleton;
 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var gov1 = Government.GetInstance();
+        var gov2 = Government.GetInstance();
+
+        Console.WriteLine(gov1 == gov2);
+    }
+}
